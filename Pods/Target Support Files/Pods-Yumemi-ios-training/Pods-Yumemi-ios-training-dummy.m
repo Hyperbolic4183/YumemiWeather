42b93eb58f1a19f746a0f18bf72e6b027cc495fe
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-08-16 08:29:10 +0900

    cocoaPodsをインストール

---
+++ Pods/Target Support Files/Pods-Yumemi-ios-training/Pods-Yumemi-ios-training-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Yumemi_ios_training : NSObject
+@end
+@implementation PodsDummy_Pods_Yumemi_ios_training
+@end
---
+++ Pods/Target Support Files/Pods-Yumemi-ios-trainingTests/Pods-Yumemi-ios-trainingTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Yumemi_ios_trainingTests : NSObject
+@end
+@implementation PodsDummy_Pods_Yumemi_ios_trainingTests
+@end
