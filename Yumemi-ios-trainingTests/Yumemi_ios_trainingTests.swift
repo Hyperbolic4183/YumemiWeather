768bedf7175c60d49724a7251ab434a8672aa0d6
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-08-17 21:08:11 +0900

    テストを追加した。

--- Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
+++ Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
@@ -29,37 +29,32 @@ class TestWeatherModel: Fetchable {
 }
 
 class Yumemi_ios_trainingTests: XCTestCase {
-    
     func test_天気予報がsunnyだったときに画面に晴れ画像が表示される() {
         let sunnyWeatherInformation = WeatherInformation(weather: .sunny, minTemperature: "0", maxTemperature: "0")
-        let sunnyModel = TestWeatherModel(weatherInformation: sunnyWeatherInformation)
-        
-        let viewController = WeatherViewController(model: sunnyModel)
-        let view = viewController.weatherView
-        let imageView = view.weatherImageView
+        let viewController = WeatherViewController(model: TestWeatherModel(weatherInformation: sunnyWeatherInformation))
+        let weatherImageView = viewController.weatherView.weatherImageView
         let expectation: XCTestExpectation? = self.expectation(description: "fetch")
-
+        
         viewController.viewDidLoad()
         viewController.weatherModel.fetch() {
             DispatchQueue.main.async {
-                XCTAssertEqual(imageView.image, UIImage(named: "sunny"))
+                XCTAssertEqual(weatherImageView.image, UIImage(named: "sunny"))
                 expectation?.fulfill()
             }
+            
         }
         waitForExpectations(timeout: 3, handler: nil)
     }
     func test_天気予報がcloudyだったときに画面に曇り画像が表示される() {
         let cloudyWeatherInformation = WeatherInformation(weather: .cloudy, minTemperature: "0", maxTemperature: "0")
-        let cloudyModel = TestWeatherModel(weatherInformation: cloudyWeatherInformation)
-        let viewController = WeatherViewController(model: cloudyModel)
-        let view = viewController.weatherView
-        let imageView = view.weatherImageView
+        let viewController = WeatherViewController(model: TestWeatherModel(weatherInformation: cloudyWeatherInformation))
+        let weatherImageView = viewController.weatherView.weatherImageView
         let expectation: XCTestExpectation? = self.expectation(description: "fetch")
-
+        
         viewController.viewDidLoad()
         viewController.weatherModel.fetch() {
             DispatchQueue.main.async {
-                XCTAssertEqual(imageView.image, UIImage(named: "cloudy"))
+                XCTAssertEqual(weatherImageView.image, UIImage(named: "cloudy"))
                 expectation?.fulfill()
             }
         }
@@ -67,16 +62,14 @@ class Yumemi_ios_trainingTests: XCTestCase {
     }
     func test_天気予報がrainyだったときに画面に雨画像が表示される() {
         let rainyWeatherInformation = WeatherInformation(weather: .rainy, minTemperature: "0", maxTemperature: "0")
-        let rainyModel = TestWeatherModel(weatherInformation: rainyWeatherInformation)
-        let viewController = WeatherViewController(model: rainyModel)
-        let view = viewController.weatherView
-        let imageView = view.weatherImageView
+        let viewController = WeatherViewController(model: TestWeatherModel(weatherInformation: rainyWeatherInformation))
+        let weatherImageView = viewController.weatherView.weatherImageView
         let expectation: XCTestExpectation? = self.expectation(description: "fetch")
-
+        
         viewController.viewDidLoad()
         viewController.weatherModel.fetch() {
             DispatchQueue.main.async {
-                XCTAssertEqual(imageView.image, UIImage(named: "rainy"))
+                XCTAssertEqual(weatherImageView.image, UIImage(named: "rainy"))
                 expectation?.fulfill()
             }
         }
@@ -88,18 +81,18 @@ class Yumemi_ios_trainingTests: XCTestCase {
         let maxTemperatureModel = TestWeatherModel(weatherInformation: maxTemperatureWeatherInformation)
         let viewController = WeatherViewController(model: maxTemperatureModel)
         let view = viewController.weatherView
+        let maxTemperature = view.maxTemperatureLabel
         let expectation: XCTestExpectation? = self.expectation(description: "fetch")
-
+        
         viewController.viewDidLoad()
         viewController.weatherModel.fetch() {
             DispatchQueue.main.async {
-                XCTAssertEqual(testingMaxTemperature,view.maxTemperatureLabel.text)
+                XCTAssertEqual(maxTemperature.text, testingMaxTemperature)
                 expectation?.fulfill()
             }
         }
         waitForExpectations(timeout: 3, handler: nil)
     }
-
     func test_最低気温がUILabelに反映される() {
         let testingMinTemperature = "-40"
         let minTemperatureWeatherInformation = WeatherInformation(weather: .sunny, minTemperature: testingMinTemperature, maxTemperature: "0")
