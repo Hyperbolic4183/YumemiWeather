0f20cbb93e21c20bd7bdee28179f360544214407
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-08-18 19:42:48 +0900

    Podsをgitの管理から除外

--- Pods/Target Support Files/Pods-Yumemi-ios-training/Pods-Yumemi-ios-training-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Yumemi_ios_training : NSObject
-@end
-@implementation PodsDummy_Pods_Yumemi_ios_training
-@end
--- Pods/Target Support Files/Pods-Yumemi-ios-trainingTests/Pods-Yumemi-ios-trainingTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Yumemi_ios_trainingTests : NSObject
-@end
-@implementation PodsDummy_Pods_Yumemi_ios_trainingTests
-@end
