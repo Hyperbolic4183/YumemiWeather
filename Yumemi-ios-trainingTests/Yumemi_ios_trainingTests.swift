fd1ddefadb67c0dce81665d2d7c768ba10150f97
Hyperbolic4183 <otsuka.amane@gmail.com>
2021-09-03 11:40:46 +0900

    update テストの書き方を変更
    refs #33

--- Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
+++ Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
@@ -10,57 +10,50 @@ import XCTest
 
 class Yumemi_ios_trainingTests: XCTestCase {
     
-    var fetcherMock: FetcherMock!
-    var weatherViewControllerMock: WeatherViewControllerMock!
+    var successFetcherMock: SuccessFetcherMock!
+    var failedFetcherMock: FailedFetcherMock!
+    let weatherViewControllerMock = WeatherViewControllerMock()
     
     func test_天気予報がsunnyだったときに画面に晴れ画像が表示される() {
-        
-        fetcherMock = FetcherMock(weather: .sunny)
-        weatherViewControllerMock = WeatherViewControllerMock(model: fetcherMock)
-        weatherViewControllerMock.model.fetch()
-        
+        successFetcherMock = SuccessFetcherMock(weatherInformation: WeatherInformation(weather: .sunny, minTemperature: "", maxTemperature: ""))
+        successFetcherMock.delegate = weatherViewControllerMock
+        successFetcherMock.fetch()
         let weatherImageView = getImage(from: weatherViewControllerMock.view)
         XCTAssertEqual(weatherImageView, UIImage(named: "sunny"))
     }
-    
-    func test_天気予報がcloudyだったときに画面に曇り画像が表示される() {
 
-        fetcherMock = FetcherMock(weather: .cloudy)
-        weatherViewControllerMock = WeatherViewControllerMock(model: fetcherMock)
-        weatherViewControllerMock.model.fetch()
-        
+    func test_天気予報がcloudyだったときに画面に曇り画像が表示される() {
+        successFetcherMock = SuccessFetcherMock(weatherInformation: WeatherInformation(weather: .cloudy, minTemperature: "", maxTemperature: ""))
+        successFetcherMock.delegate = weatherViewControllerMock
+        successFetcherMock.fetch()
         let weatherImageView = getImage(from: weatherViewControllerMock.view)
         XCTAssertEqual(weatherImageView, UIImage(named: "cloudy"))
     }
 
     func test_天気予報がrainyだったときに画面に雨画像が表示される() {
-        fetcherMock = FetcherMock(weather: .rainy)
-        weatherViewControllerMock = WeatherViewControllerMock(model: fetcherMock)
-        weatherViewControllerMock.model.fetch()
-        
+        successFetcherMock = SuccessFetcherMock(weatherInformation: WeatherInformation(weather: .rainy, minTemperature: "", maxTemperature: ""))
+        successFetcherMock.delegate = weatherViewControllerMock
+        successFetcherMock.fetch()
         let weatherImageView = getImage(from: weatherViewControllerMock.view)
         XCTAssertEqual(weatherImageView, UIImage(named: "rainy"))
     }
 
     func test_最高気温がUILabelに反映される() {
         let testMaxTemperature = "40"
-        fetcherMock = FetcherMock(maxTemperature: testMaxTemperature)
-        weatherViewControllerMock = WeatherViewControllerMock(model: fetcherMock)
-        weatherViewControllerMock.model.fetch()
-        
+        successFetcherMock = SuccessFetcherMock(weatherInformation: WeatherInformation(weather: .sunny, minTemperature: "", maxTemperature: testMaxTemperature))
+        successFetcherMock.delegate = weatherViewControllerMock
+        successFetcherMock.fetch()
         let maxTemperature = self.getMaxTemperatureLabel(from: weatherViewControllerMock.view)
-       
+
         XCTAssertEqual(testMaxTemperature, maxTemperature.text)
     }
 
     func test_最低気温がUILabelに反映される() {
         let testMinTemperature = "-40"
-        fetcherMock = FetcherMock(maxTemperature: testMinTemperature)
-        weatherViewControllerMock = WeatherViewControllerMock(model: fetcherMock)
-        weatherViewControllerMock.model.fetch()
-        
-        let minTemperature = self.getMaxTemperatureLabel(from: weatherViewControllerMock.view)
-       
+        successFetcherMock = SuccessFetcherMock(weatherInformation: WeatherInformation(weather: .sunny, minTemperature: testMinTemperature, maxTemperature: ""))
+        successFetcherMock.delegate = weatherViewControllerMock
+        successFetcherMock.fetch()
+        let minTemperature = self.getMinTemperatureLabel(from: weatherViewControllerMock.view)
         XCTAssertEqual(testMinTemperature, minTemperature.text)
     }
     
