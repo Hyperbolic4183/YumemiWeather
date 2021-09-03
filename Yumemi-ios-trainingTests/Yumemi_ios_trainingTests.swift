0105255ac9dad9c5c4b2b821776f2f5a6ca7d759
Hyperbolic4183 <otsuka.amane@gmail.com>
2021-09-03 11:36:59 +0900

    update XCTExpectationを拡張
    エラーが起きた際に処理する関数が実行されることを確認する時に用いるプロパティを定義

--- Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
+++ Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
@@ -99,9 +99,9 @@ class Yumemi_ios_trainingTests: XCTestCase {
     private func getReloadButton(from view: UIView) -> UIButton {
         view.subviews.filter({ type(of: $0) == UIButton.self }).map({ $0 as! UIButton })[1]
     }
-}
-
     
 }
 
+extension XCTestExpectation {
+    static let expectationOfError = XCTestExpectation(description: "error")
 }
