c99d96b2f2c366745d9f9d70d4ad9398841bce71
Hyperbolic4183 <otsuka.amane@gmail.com>
2021-09-13 16:58:00 +0900

    fix 強制キャストを無くした。
    refs #33

--- Yumemi-ios-training/Controller/ErrorHandler.swift
+++ Yumemi-ios-training/Controller/ErrorHandler.swift
@@ -8,27 +8,23 @@
 import UIKit
 
 struct ErrorHandler {
-    var handle: (_ viewController: UIViewController, _ error: Error) -> Void
-}
-
-fileprivate func extractedFunc(_ error: Error, _ viewController: UIViewController) {
-    let message: String
-    switch error as! WeatherAppError {
-    case .invalidParameterError:
-        message = "不適切な値が設定されました"
-    case .unknownError:
-        message = "予期せぬエラーが発生しました"
-    }
-    let errorAlert = UIAlertController(title: "エラー", message: message, preferredStyle: .alert)
-    let errorAction = UIAlertAction(title: "OK", style: .default)
-    errorAlert.addAction(errorAction)
-    viewController.present(errorAlert, animated: true, completion: nil)
+    var handle: (_ viewController: WeatherViewController, _ error: WeatherAppError) -> Void
 }
 
 extension ErrorHandler {
     static let presentAlertViewController = Self(
         handle: { viewController, error in
-            extractedFunc(error, viewController)
+            let message: String
+            switch error {
+            case .invalidParameterError:
+                message = "不適切な値が設定されました"
+            case .unknownError:
+                message = "予期せぬエラーが発生しました"
+            }
+            let errorAlert = UIAlertController(title: "エラー", message: message, preferredStyle: .alert)
+            let errorAction = UIAlertAction(title: "OK", style: .default)
+            errorAlert.addAction(errorAction)
+            viewController.present(errorAlert, animated: true, completion: nil)
         }
     )
 }
