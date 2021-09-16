2f9d8563372e7532a28c97f1ffaaf33cdded218a
Hyperbolic4183 <otsuka.amane@gmail.com>
2021-09-16 16:26:46 +0900

    update fetch()の引数の変化に伴い、処理を変更した

--- Yumemi-ios-trainingTests/FetcherMock.swift
+++ Yumemi-ios-trainingTests/FetcherMock.swift
@@ -8,20 +8,13 @@
 @testable import Yumemi_ios_training
 class FetcherMock: Fetchable {
     
-    var delegate: FetchableDelegate?
-    
     let result: Result<WeatherInformation, WeatherAppError>
     init(result: Result<WeatherInformation, WeatherAppError>) {
         self.result = result
     }
     
-    func fetch() {
-        switch result {
-        case .success(let weatherInformation):
-            delegate?.fetch(self, didFetch: weatherInformation)
-        case .failure(let error):
-            delegate?.fetch(self, didFailWithError: error)
-        }
+    func fetch(completion: @escaping (Result<WeatherInformation, WeatherAppError>) -> Void) {
+        completion(result)
     }
     
 }
